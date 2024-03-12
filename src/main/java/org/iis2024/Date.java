ec8cef1e0bdca7a3d6f6a8cd5515207a8c288e1c
sergiovargasm25 <s.vargas.m@uma.es>
2024-03-12 10:26:00 +0100

    Updated error

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -86,8 +86,8 @@ public class Date {
       return false;
     }
   }
+  public void methodAddedByOwner(){
+    System.out.println("Method added by the owner");com
+  }
 }
 
-  public void methodAddedByOwner(){
-    System.out.println("Method added by the owner")
-  }
